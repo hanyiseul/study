79783a4ae4f2c6b88ee8c961f61c0798f1d92967
hanyiseul <seulli_hys@daum.net>
2026-05-28 10:28:18 +0900

    FileReader FileWriter 기반 파일 처리 실습

---
+++ java/basic/src/ByteStream/BufferCopy.java
@@ -0,0 +1,26 @@
+package ByteStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BufferCopy {
+    public static void main(String[] args) {
+        try (
+                FileInputStream input = new FileInputStream("source.dat");
+                FileOutputStream output = new FileOutputStream("copy.dat")
+        ) {
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = input.read(buffer)) != -1) {
+                output.write(buffer, 0, length);
+            }
+
+            System.out.println("파일 복사가 완료되었습니다.");
+        } catch (IOException e) {
+            System.out.println("파일 복사 중 오류가 발생했습니다.");
+            System.out.println(e.getMessage());
+        }
+    }
+}
