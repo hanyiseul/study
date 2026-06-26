2c5cdb77eea06576892aa95ec4878b057515b841
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-06-26 18:53:11 +0900

    모바일 뱅크 코드 분석

--- java/redis/redis-distributed-demo/src/main/java/com/example/demo/config/CacheConfig.java
+++ java/redis/redis-distributed-demo/src/main/java/com/example/demo/config/CacheConfig.java
@@ -1,11 +1,13 @@
+// Redis 캐시의 설정을 담당하는 클래스
+// 캐시에 저장되는 데이터는 60초 동안만 유지하고, 그 이후에는 자동으로 삭제해라 라는 규칙을 설명
 package com.example.demo.config;
 
-import java.time.Duration;
+import java.time.Duration; // 시간 표현
 
-import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer; // Redis ChachManager를 수정할 수 있게 해주는 인터페이스 (Redis 캐시 설정을 내가 직접 바꾸겠다)
+import org.springframework.context.annotation.Bean; // Bean 등록을 위한 어노테이션 -> Spring이 객체를 생성해서 관리하게 됨
+import org.springframework.context.annotation.Configuration; // 설정 클래스 -> Spring Boot가 실행될 때 가장 먼저 읽음
+import org.springframework.data.redis.cache.RedisCacheConfiguration; //
 
 @Configuration
 public class CacheConfig {
--- java/redis/redis-distributed-demo/src/main/java/com/example/demo/product/Product.java
+++ java/redis/redis-distributed-demo/src/main/java/com/example/demo/product/Product.java
@@ -1,11 +1,13 @@
+// product entity 파일
+// 데이터베이스 테이블
 package com.example.demo.product;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue; // id를 자동 생성할 수 있게 해주는 어노테이션
+import jakarta.persistence.GenerationType; // GeneratedValue에서 어떤 방식으로 자동 생성할지 선택
+import jakarta.persistence.Id; // pk 알려주는거 (PRIMARY KEY(id))
 
-import java.io.Serializable;
+import java.io.Serializable; // 객체를 직렬화할 수 있게 하는 인터페이스 (Redis, 캐시, 세션 등에 저장할 때 많이 사용)
 
 @Entity
 public class Product implements Serializable {
